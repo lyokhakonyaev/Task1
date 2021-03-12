ba9b0e07b1f108164b6808a247f1ac3065d260c4
AlexeyKonyaev <lyokha.konyaev@gmail.com>
2021-03-12 09:34:00 +0300

    Rate buttons bug fixed, almost finished

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const webpack = require('webpack');
+const webpack = require('webpack')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
